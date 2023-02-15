14754a51e4387fe05dfcab5cf4251d1241a73937
geunsoo513 <80021360+geunsoo513@users.noreply.github.com>
2023-02-15 20:54:54 +0900

    서로 다른 데이터 형 변환

--- My project (1)/Assets/Exercise01.cs
+++ My project (1)/Assets/Exercise01.cs
@@ -6,6 +6,118 @@ public class Exercise01 : MonoBehaviour
 {
     private void Awake()
     {
+        string stringValue = "초기화";
+        int intValue = 33;
+        float floatValue = 12.3456f;
+
+        Debug.Log("stringValue : " + stringValue);
+
+        //정수를 문자열로 형 변환
+        stringValue = intValue.ToString();
+        Debug.Log("stringValue : " + stringValue);
+
+        //실수를 문자열로 형 변환
+        stringValue = floatValue.ToString();
+        Debug.Log("stringValue : " + stringValue);
+
+
+        /*문자열을 숫자로 형 변환 2 -TryParse()
+        string stringValue = "숫자가 아닙니다.";
+        int intValue = 10;
+
+        bool isConversation = int.TryParse(stringValue, out intValue);
+        if (isConversation == true)
+        {
+            Debug.Log("stringValue : " + stringValue);
+            Debug.Log("intValue : " + intValue);
+
+        }
+        else
+        {
+            Debug.Log("stringValue 변수에 들어있는 내용이 숫자가 아니어서 형 변환에 실패했다.");
+        }
+
+        //문자열에 숫자가 아닌 데이터가 들어있을 경우 숫자로 형 변환을 시도하면 에러가 발생함.
+        /* 문자열을 숫자로 형 변환 1 - Parse()
+        int intValue = 10;
+        float floatValue = 12.3456f;
+        string stringValue = "33";
+
+        Debug.Log("intValue : " + intValue);
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("stringValue : " + stringValue);
+
+        //33.4567과 같은 실수를 int로 형 변환을 시도하면 에러가 발생함.
+        intValue = int.Parse(stringValue);
+        stringValue="33.4567";
+        floatValue = float.Parse(stringValue);
+
+        Debug.Log("intValue : " + intValue);
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("stringValue : " + stringValue);
+
+        /* 부동 소수점과 정수 사이의 형 변환
+        float floatValue = 0.9f;
+        int intValue = (int)floatValue;
+
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("intValue : " + intValue);
+
+        floatValue = 1.1f;
+        intValue = (int)floatValue;
+
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("intValue : " + intValue);
+
+
+        /*크기가 서로 다른 부동 소수점 사이의 형 변환
+        float floatValue = 69.6875f;
+        double doubleValue = (double)floatValue;
+
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("doubleValue : " + doubleValue);
+
+        floatValue = 0.1f;
+        doubleValue = (double)floatValue;
+
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("doubleValue : " + doubleValue);
+           
+
+        /*부호 있는 정수와 부호 없는 정수 사이의 형 변환
+        sbyte sbyteValue = 31;
+        byte byteValue = (byte)sbyteValue;
+        Debug.Log("sbyteValue : " + sbyteValue);
+        Debug.Log("byteValue : " + byteValue);
+
+        //부호 있는 정수가 음수일 때
+        sbyteValue = -31;
+        byteValue = (byte)sbyteValue;
+
+        Debug.Log("sbyteValue : " + sbyteValue);
+        Debug.Log("byteValue : " + byteValue);*/
+
+        //부호 없는 정수가 부호 있는 정수 최대 값보다 클 때
+        /*byteValue = 200;
+        sbyteValue = (sbyte)byteValue;
+
+        Debug.Log("sbyteValue : " + sbyteValue);
+        Debug.Log("byteValue : " + byteValue);*/
+
+        /* 크기가 서로 다른 정수 사이의 형 변환
+        sbyte sbyteValue = 10;
+        int intValue = (int)sbyteValue;
+
+        Debug.Log("sbyteValue : " + sbyteValue);
+        Debug.Log("intValue : " + intValue);
+
+        intValue = 130;
+        sbyteValue = (sbyte)intValue;
+
+        Debug.Log("sbyteValue : " + sbyteValue);
+        Debug.Log("intValue : " + intValue);*/
+
+        /* 오브젝트 변수
         object valueInt = 31;//박싱 == 데이터저장
         object valueFloat=3.14159265358979f;
         object valueString = "객체지향 프로그래밍";
@@ -14,7 +126,7 @@ public class Exercise01 : MonoBehaviour
         Debug.Log("정수 : " + valueInt);//언박싱 == 데이터사용
         Debug.Log("실수 : " + valueFloat);
         Debug.Log("문자열 : " + valueString);
-        Debug.Log("논리 : " + valueBool);
+        Debug.Log("논리 : " + valueBool);*/
 
 
         /*문자열, 논리
