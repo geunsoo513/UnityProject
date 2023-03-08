71f4c855489d80f90989c24ab5b20a27c773541e
geunsoo513 <80021360+geunsoo513@users.noreply.github.com>
2023-03-08 22:17:51 +0900

    오브젝트 이동
    CharacterController로 오브젝트 이동 연습

---
+++ CharacterController/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField]
+    private KeyCode jumpKeyCode = KeyCode.Space;
+    [SerializeField]
+    private CameraController cameraController;
+    private Movement3D movement3D;
+
+    private void Awake()
+    {
+        movement3D = GetComponent<Movement3D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        movement3D.MoveTo(new Vector3(x, 0, z));
+        if (Input.GetKeyDown(jumpKeyCode))
+        {
+            movement3D.JumpTo();
+        }
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        cameraController.RoateTo(mouseX, mouseY);
+    }
+}
